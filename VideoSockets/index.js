93441ec8d4b5cffe9cbdacdcdc9b502a10b5aaf3
TrimBallanca <trim.ballanca@gmail.com>
2018-04-11 01:09:43 -0400

    Added switch statement for messages to server

--- VideoSockets/index.js
+++ VideoSockets/index.js
@@ -10,19 +10,48 @@ const server = http.createServer(app);
 //initialize the WebSocket server instance
 const wss = new WebSocket.Server({ server });
 
-wss.on('connection', (ws) => {
+const successful = {"success" : true};
+var MessageTypeEnum = Object.freeze({"play":1, "pause":2, "leave":3, "join":4, "":5})
 
+//we expect a message with connection open
+//incoming message contains sessionID
+wss.on('connection', (ws) => {
     //connection is up, let's add a simple simple event
     ws.on('message', (message) => {
 
-        //log the received message and send it back to the client
+        //log the received sessionID and send a "success" message to the client
         console.log('received: %s', message);
-        ws.send(`Hello, you sent -> ${message}`);
+        ws.send(successful);
     });
-
-    //send immediatly a feedback to the incoming connection
-    ws.send('Hi there, I am a WebSocket server');
 });
+
+
+ws.on('message', (message) => {
+    //message contains one type of message
+    switch (message.type) {
+        case MessageTypeEnum.play:
+        case MessageTypeEnum.pause:
+            //call broaast pause
+            break;
+        case MessageTypeEnum.leave:
+            //call leave session
+            break;
+        case MessageTypeEnum.join:
+            //call join session
+            break;
+        default:
+            //we shouldn't have gotten here
+            ws.send("Can't read message type");
+    }
+    console.log('received: %s', message);
+    ws.send(`Hello, you sent -> ${message}`);
+});
+
+function broadcast(sessionID, mode) {
+  console.log(myNickname);
+  console.log(myCurrentPage);
+  console.log(mySessionID);
+}
 
 //start our server
 server.listen(process.env.PORT || 8080, () => {
