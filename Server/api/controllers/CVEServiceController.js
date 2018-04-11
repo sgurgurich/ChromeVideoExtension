4c677a2654e1af1af6bbe84ea90ba43021431271
John <john@vps.bellisimospizza.com>
2018-04-11 12:05:29 -0400

    Worked on random session id generation

--- Server/api/controllers/CVEServiceController.js
+++ Server/api/controllers/CVEServiceController.js
@@ -76,6 +76,37 @@ exports.does_user_exist = function(req, res) {
 };
 
 exports.generate_session = function(req, res) {
+  var session_id = generate_random_id();
+  
+  var my_session_id = generate_uniquue_session(session_id);
+  	    
+  var my_session = new Session({sessionId: my_session_id});
+  console.log(my_session);
+  my_session.save(function(err, session){
+    if(err) res.send(err);
+    res.json(session);
+  });
+}
+
+function generate_uniquue_session(session_id){
+  return Session
+    .findOne({sessionId: session_id})
+    .then(function(session){
+      if(session) {
+	console.log("Session found, trying again");
+	new_id = generate_random_id();
+	return generate_uniquue_session(new_id);
+      }
+      console.log("Found unique id");
+      return session_id;
+    })
+    .catch(function(err){
+      console.log(error);
+    });
+}
+
+function generate_random_id()
+{
   var firstChar = (Math.floor(Math.random() * 10)).toString();
   var secondChar = (Math.floor(Math.random() * 10)).toString();
   var thirdChar = (Math.floor(Math.random() * 10)).toString();
@@ -85,11 +116,5 @@ exports.generate_session = function(req, res) {
   var seventhChar = (Math.floor(Math.random() * 10)).toString();
   var eighthChar = (Math.floor(Math.random() * 10)).toString();
    
-  var session_id = firstChar + secondChar + thirdChar + fourthChar + fifthChar + sixthChar + seventhChar + eighthChar;
-    
- 
-  var my_session = new Session({sessionId: session_id});
-  my_session.save(function(err) {
-    res.json({"sessionId": session_id});
-  });
+  return firstChar + secondChar + thirdChar + fourthChar + fifthChar + sixthChar + seventhChar + eighthChar;
 }
