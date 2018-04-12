e35d2c125f7819518eec275d41df13d405f94ccc
TrimBallanca <trim.ballanca@gmail.com>
2018-04-11 23:26:43 -0400

    index.js fixed to handle multiple connections

--- VideoSockets/index.js
+++ VideoSockets/index.js
@@ -10,70 +10,51 @@ const server = http.createServer(app);
 //initialize the WebSocket server instance
 const wss = new WebSocket.Server({ server });
 
+
+
 //variables
 var successful = {"success" : true};
-var MessageTypeEnum = Object.freeze({"play":1, "pause":2, "leave":3, "join":4, "update":5});
+var MessageTypeEnum = Object.freeze({"play":1, "pause":2, "leave":3, "join":4, "update":5, "newconnection":6});
+var CLIENTS = {};
+var SESSIONS = {};
 
-//key: sessionID
-//value: array of users currently connected
-var sessions = {};
-
-
-
-//we expect a message with connection open
-//incoming message contains sessionID
 wss.on('connection', (ws) => {
-    //connection is up
-    ws.on('message', (message) => {
-        //log the received sessionID and send a "success" message to the client
-        console.log('received: %s', message);
-        ws.send(successful);
+    //connection is up, let's add a simple simple event
+        ws.on('message', (theMessage) => {
+        message = JSON.parse(theMessage);
+        console.log(message);
+        console.log(message.type);
+        switch (message.type){
+            case "newconnection":
+                ws.send("you're a new connection");
+                createNewConnection(ws,message.sessionID,message.userID);
+                broadcastUpdateGUI(message.sessionID);
+                break;
+            case "update":
+                break;
+            default:
+                ws.send("how did you get here..?");
+        }
+        //log the received message and send it back to the client
     });
 });
 
 
-ws.on('message', (message) => {
-    //message contains one type of message
-    switch (message.type) {
-        case MessageTypeEnum.play:
-        case MessageTypeEnum.pause:
-            //call broadcast pause
-            break;
-        case MessageTypeEnum.leave:
-            //call leave session
-            break;
-        case MessageTypeEnum.join:
-            //call join session
-            message.userid;
-            message.sessionid;
-            break;
-        case MessageTypeEnum.update:
-            //broadcast that there has been an update to session
-            //this should initiation requerying database on GUI
-            break;
-        default:
-            //we shouldn't have gotten here
-            ws.send("Can't read message type");
-    }
-});
-
-function broadcast(sessionID, mode) {
+function createNewConnection(ws,sessionID,userID) {
+        //store the userID and sessionID
+        CLIENTS[userID] = ws;
+        if(sessionID in SESSIONS) {
+            SESSIONS[sessionID] = SESSIONS[sessionID].push(ws);
+        }
+        else {
+            SESSIONS[sessionID] = [ws];
+        }
 }
-
-function join(userid, sessionid) 
-{
-    if(sessions[sessionid] != null)
-    {
-        sessions[sessionid] = sessions[sessionid].push(userid);
+function broadcastUpdateGUI(sessionID) {
+    var clients = SESSIONS[sessionID];
+    for (var i=0; i<clients.length; i++) {
+        clients[i].send("updateAlert");
     }
-    else
-    {
-        sessions[sessionid] = [userid];
-    }
-    return true;
-}
-
-function leave(sessionID, mode) {
 }
 
 //start our server
